d6b8c6c0fea2fdf02f12dcf90c687610cd6ba51c
Islam <islam.fawzy25@hotmail.com>
2023-01-16 13:29:47 +0100

    add courses seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+courses = Course.create(
+   [
+   {title:"Hello World first course",description:"This is the first course",author:"Frank Brown",url:"https://www.youtube.com/embed/dROHxWfRvBI"},
+   {title:"Hello World second course",description:"This is the second course",author:"Adam smith",url:"https://www.youtube.com/embed/dROHxWfRvBI"}
+   ]
+)
+
  episodes = Episode.create([ 
-    {title:"epsiod 1",description:"epsiod 1 description",url:"https://www.youtube.com/embed/dROHxWfRvBI",course_id:1},
-    {title:"epsiod 2",description:"epsiod 2 description",url:"https://www.youtube.com/embed/dROHxWfRvBI",course_id:1},
+    {title:"epsiod 1- course 1",description:"epsiod 1 description",url:"https://www.youtube.com/embed/dROHxWfRvBI",course_id:1},
+    {title:"epsiod 2- course 1",description:"epsiod 2 description",url:"https://www.youtube.com/embed/dROHxWfRvBI",course_id:1},
     {title:"epsiod 1- course 2",description:"epsiod 1 description",url:"https://www.youtube.com/embed/dROHxWfRvBI",course_id:2},
     {title:"epsiod 2 - course 2",description:"epsiod 2 description",url:"https://www.youtube.com/embed/dROHxWfRvBI",course_id:2},
 
